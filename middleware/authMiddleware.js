cd70e4d842400fde9c0bd18e98b5cc299f47ec8b
Aniket Thapa <aniketthapa04@gmail.com>
2025-01-02 00:42:22 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,3 @@
-// Middleware to save the original URL
-function saveOriginalUrl(req, res, next) {
-    if (!req.isAuthenticated() && req.method === 'GET') {
-        req.session.returnTo = req.originalUrl; // Save the URL in session
-    }
-    next();
-}
-app.use(saveOriginalUrl);
-
-
 // Middleware to check if user is authenticated
 module.exports.isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
