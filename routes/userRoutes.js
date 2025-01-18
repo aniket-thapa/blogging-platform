1749f751cec87eb96ac016ea4514970565f7b226
Aniket Thapa <aniketthapa04@gmail.com>
2025-01-18 21:22:56 +0530

    fixed bugs and errors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,20 @@ const router = express.Router();
 // User Profile Route
 router.get('/', isAuthenticated, async (req, res) => {
   try {
+    res.render('users/profile', { user: req.user });
+  } catch (err) {
+    res.redirect('/blogs');
+  }
+});
+
+// User Profile Route
+router.get('/created', isAuthenticated, async (req, res) => {
+  try {
     const userBlogs = await Blog.find({ author: req.user._id }).populate(
       'author',
       'username'
     );
-    res.render('users/profile', { user: req.user, blogs: userBlogs });
+    res.render('users/createdBlogs', { user: req.user, blogs: userBlogs });
   } catch (err) {
     res.redirect('/blogs');
   }
