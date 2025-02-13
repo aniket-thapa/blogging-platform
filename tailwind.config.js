df563c3bc75316361d5b80b2dd9180fdd67536c5
Aniket Thapa <aniketthapa04@gmail.com>
2025-02-13 19:22:31 +0530

    fixed errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./views/**/*.ejs'],
+  content: ['./views/**/*.ejs', './public/**/*.js'],
   theme: {
     extend: {
       colors: {
