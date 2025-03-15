bb8cb1c0007ad47f0676c61c421d5eb1ab8344e5
Aniket Thapa <aniketthapa04@gmail.com>
2025-03-15 18:02:38 +0530

    FIXED CODE

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,4 @@
-// Middleware to check if user is authenticated
+// Middleware to check if user is authenticated using method given by Passport.js
 module.exports.isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
